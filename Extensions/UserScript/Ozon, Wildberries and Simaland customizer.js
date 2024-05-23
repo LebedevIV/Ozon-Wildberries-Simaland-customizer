ae18dad472ecb25f4a25f3fd0b09fbd85e8fea20
Igor Lebedev <igor_samodurov@mail.ru>
2024-05-23 16:41:28 +0500

    Update Ozon, Wildberries and Simaland customizer.js

--- Extensions/UserScript/Ozon, Wildberries and Simaland customizer.js
+++ Extensions/UserScript/Ozon, Wildberries and Simaland customizer.js
@@ -2,7 +2,7 @@
 // @name         Ozon, Wildberries and Simaland customizer: sorting reviews by ascending rating
 // @name:ru      Ozon, Wildberries и Simaland настройка: сортировка отзывов по возрастанию рейтинга
 // @namespace    http://tampermonkey.net/
-// @version      2024-05-21
+// @version      2024-05-23
 // @description  Ozon, Wildberries and Simaland: sorting reviews by product by ascending rating
 // @description:ru  Ozon, Wildberries и Simaland: сортировка отзывов по товару по возрастанию рейтинга
 // @author       Igor Lebedev
@@ -25,7 +25,33 @@
     // получаем текущий адрес страницы
     const currentURL = window.location.href
 
-
+    // Ozon: Функция для добавления к ссылкам параметра сортировки рейтинга по возрастанию - на случай если пользователь будет вручную открывать ссылки с карточкой товара в новой вкладке
+    function addOzonSortParamToLinks() {
+		if (config.SettingsOnOff) {
+			const links = document.querySelectorAll('a[href^="/product/"]:not([href*="&sort=score_asc"])');
+			links.forEach(link => {
+				const linkOrig = link.href
+				link.href += '&sort=score_asc';
+				// Проверяем, является ли родительский элемент (parentNode) div с классом 'iy6'
+				const link_parentNode = link.parentNode
+				// Привязка к блоку рейтингов (звёздочек) ссылки на рейтинги
+				if(link_parentNode.tagName.toLowerCase() === 'div' && link_parentNode.classList.contains('iy6')) {
+					// Определение наличия вложенного элемента, содержащего рейтинги
+					var divStars = link_parentNode.querySelector('div.iw.wi1');
+					if (divStars) {
+						// Сохранение текущего содержимого div
+						let oldHTML = divStars.innerHTML;
+						// Оборачивание существующего содержимого div в собственную ссылку
+						// и присвоение стиля 'cursor: pointer'
+						// привязка полученного href к текущему div + добавление к ссылке метки в виде трёх символов якоря, которые не удаляется из строки
+						let url1Base = linkOrig.match(/(^[^\?]+)/g)[0];
+						divStars.innerHTML = `<a href="${url1Base}reviews?sort=score_asc" style="display: flex; width: 100%; height: 100%; cursor: pointer;">${oldHTML}</a>`;
+					}
+				}					
+			});
+		}
+    }
+	
     // Wildberries: Ожидание загружки страницы товара до появления элемента сортировки рейтинга и искусственное двойное нажатие этого элемента чтобы добиться сортировки рейтинга по возрастанию
     function sortWildberriesReviews() {
         const interval = setInterval(() => {
@@ -191,10 +217,25 @@
 
 
     // Проверка, является ли страница карточкой товара, содержащей отзывы, и если да - сортировка отзывов по возрастанию рейтинга. В случае Simalend важна последовательность
-    // Ozon: начинается ли адрес страницы со 'https://www.ozon.ru/product/' и не содержит ли он уже '&sort=score_asc'
-    if (currentURL.includes('ozon.ru/product/') && !currentURL.includes('&sort=score_asc')) {
+    // Ozon: начинается ли адрес страницы со 'https://www.ozon.ru/product/' и не содержит ли он уже '&sort=score_asc' и прочие варианты сортировки
+    if (currentURL.includes('ozon.ru/product/') && !currentURL.includes('&sort=score_asc') && !currentURL.includes('?sort=score_asc') && !currentURL.includes('&sort=score_desc') && !currentURL.includes('?sort=score_desc')) {
         // Если условия выполняются - добавляем к адресу параметр и перезагружаем страницу с новым адресом, производящим сортировку рейтингов по возрастанию
-        window.location.href ||= `${currentURL}&sort=score_asc`;
+        if (config.SettingsOnOff) {
+			let NewURL
+			if (!currentURL.includes('reviews?sort=score_asc') && !currentURL.includes('reviews?sort=score_desc')) {
+				if (currentURL.includes('/reviews')) {
+					NewURL = currentURL.replace('/reviews', 'reviews?sort=score_asc');
+				} else {
+					NewURL = `${currentURL}&sort=score_asc`;				
+				}
+			}
+			chrome.runtime.sendMessage({action: "redirect", url: NewURL});
+		}
+    // Ozon: Страница каталога товаров
+    } else if (currentURL.includes('ozon.ru/category/') ) {
+        // Если условия выполняются - добавляем к адресу параметр и перезагружаем страницу с новым адресом, производящим сортировку рейтингов по возрастанию
+        if (config.SettingsOnOff) 
+			addOzonSortParamToLinks()
         // Wildberries:
     } else if (currentURL.includes('wildberries.ru/catalog/') && currentURL.includes('/feedbacks?imtId=')) {
         sortWildberriesReviews();
